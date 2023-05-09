98e7d100eeab5f85bca2f9425841c8dbaf559c19
christianarca27 <christian.arca.mail@gmail.com>
2023-05-09 16:42:38 +0200

    feat: inseriti dati in pagina da comics.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     <main>
-        Contenuto
+        <div class="container d-flex flex-column align-items-center">
+            <ul id="comics-list" class="row g-5">
+                @foreach ($comics as $comic)
+                    <li class="comic col-2">
+                        <img class="comic-img" src="{{ $comic['thumb'] }}" alt="">
+                        <span class="comic-title">{{ $comic['series'] }}</span>
+                    </li>
+                @endforeach
+            </ul>
+
+            <button>LOAD MORE</button>
+        </div>
     </main>
 @endsection
